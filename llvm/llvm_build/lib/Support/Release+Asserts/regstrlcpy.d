324005f5b7671257115be01479b6de614b8e6578
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-03-18 14:18:36 -0400

    Add more cases

--- llvm/llvm_build/lib/Support/Release+Asserts/regstrlcpy.d
+++
@@ -1,6 +0,0 @@
-/Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm_build/lib/Support/Release+Asserts/regstrlcpy.o \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm_build/lib/Support/Release+Asserts/regstrlcpy.d: \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/lib/Support/regstrlcpy.c \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/lib/Support/regex_impl.h
-
-/Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/lib/Support/regex_impl.h:
