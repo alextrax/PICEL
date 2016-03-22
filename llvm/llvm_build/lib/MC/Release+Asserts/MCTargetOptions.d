e99d382bab8628a5ec56b2c45824b693d4d1f9b9
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-03-22 19:40:59 -0400

    Delete llvm folder

--- llvm/llvm_build/lib/MC/Release+Asserts/MCTargetOptions.d
+++
@@ -1,9 +0,0 @@
-/Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm_build/lib/MC/Release+Asserts/MCTargetOptions.o \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm_build/lib/MC/Release+Asserts/MCTargetOptions.d: \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/lib/MC/MCTargetOptions.cpp \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/include/llvm/ADT/StringRef.h \
-  /Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/include/llvm/MC/MCTargetOptions.h
-
-/Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/include/llvm/ADT/StringRef.h:
-
-/Users/chia-haohsu/GoogleDrive/CSgod/CUcourses/PLT_Programming_Language_And_Translator/PICEL_Proj/final_proj/llvm/llvm-3.7.1.src/include/llvm/MC/MCTargetOptions.h:
