0844eadb7d97a911b89584e4f5ead37f4cae45db
alextrax <luciferxiii@gmail.com>
2016-03-23 00:39:04 -0400

    Support AST tree printing

--- microc-llvm/ast.ml
+++ microc-llvm/ast.ml
@@ -52,9 +52,40 @@ type decl = Vdecl of vdecl
 type  program = decl list
 
 
+(* test print *)
+(*let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+  | Void -> "void"
+
+let rec string_of_expr = function
+    StringLit(s) -> s
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+*)
+
 (* Pretty-printing functions *)
 
-(* let string_of_op = function
+ let string_of_op = function
     Add -> "+"
   | Sub -> "-"
   | Mult -> "*"
@@ -74,6 +105,7 @@ let string_of_uop = function
 
 let rec string_of_expr = function
     Literal(l) -> string_of_int l
+  | StringLit(s) -> "\"" ^ s ^ "\""
   | BoolLit(true) -> "true"
   | BoolLit(false) -> "false"
   | Id(s) -> s
@@ -93,27 +125,39 @@ let rec string_of_stmt = function
   | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
   | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
       string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
+  (*| For(e1, e2, e3, s) ->
       "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
       string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s*)
 
 let string_of_typ = function
     Int -> "int"
   | Bool -> "bool"
   | Void -> "void"
 
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_bind (t, id) =
+  string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_vdecl = function
+  Bind(bind) -> string_of_bind bind
 
 let string_of_fdecl fdecl =
   string_of_typ fdecl.typ ^ " " ^
   fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
   ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  (*String.concat "" (List.map string_of_vdecl fdecl.locals) ^*)
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "}\n"
 
-let string_of_program (vars, funcs) =
+let string_of_decl = function
+  Vdecl(vdecl) -> string_of_vdecl vdecl  ^ "\n" 
+  |Fdecl(func_decl) -> " " ^ string_of_fdecl func_decl ^ "\n"
+
+(*let string_of_decl (vars, funcs) =
   String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
- *)
+  String.concat "\n" (List.map string_of_fdecl funcs)*)
+
+let string_of_program (decls) =
+  String.concat "" (List.map string_of_decl decls)
+
