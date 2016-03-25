ef44b01216fd727a2bc06961582fd4dd5091570d
RuiLu <rlu0213@hotmail.com>
2016-03-25 11:39:22 -0400

    Micro.ml v1

--- microc-llvm/microc.ml
+++ microc-llvm/microc.ml
@@ -10,13 +10,13 @@ let _ =
             ("-c", Compile) ] (* Generate, check LLVM IR *)
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
-  let ast = Parser.program Scanner.token lexbuf(* in
-  Semant.check ast;*)
-in print_string (Ast.string_of_program ast) ;;
-(*  match action with
+  let ast = Parser.program Scanner.token lexbuf in
+  Semant.check ast;
+(* in print_string (Ast.string_of_program ast) ;; *)
+  match action with
     Ast -> print_string (Ast.string_of_program ast)
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
   | Compile -> let m = Codegen.translate ast in
     Llvm_analysis.assert_valid_module m;
     print_string (Llvm.string_of_llmodule m)
- *)
+
