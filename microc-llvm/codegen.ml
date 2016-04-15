b3cfbf5261accd2282de9c965806add99c347cbd
alextrax <luciferxiii@gmail.com>
2016-04-15 10:50:45 -0400

    Add built-in function newpic() for creating new empty pic

--- microc-llvm/codegen.ml
+++ microc-llvm/codegen.ml
@@ -91,7 +91,8 @@ let translate program =
   let ext_save_func = L.declare_function "save" ext_save_t the_module in
   let ext_save_file_t = L.var_arg_function_type i32_t [|  L.pointer_type i8_t ; pic_p|] in
   let ext_save_file_func = L.declare_function "save_file" ext_save_file_t the_module in
-
+  let ext_newpic_t = L.var_arg_function_type pic_t [| i32_t ; i32_t |] in
+  let ext_newpic_func = L.declare_function "newpic" ext_newpic_t the_module in
 
   (* Define each function (arguments and return type) so we can call it *)
   let function_decls =
@@ -304,6 +305,10 @@ let translate program =
         (match b with A.Id s ->
     L.build_call ext_save_file_func [| (expr builder a) ; (lookup s) |]
       "save_file" builder)
+      | A.Call ("newpic", e) ->
+      let a = List.hd e in let b = List.hd (List.tl e) in
+    L.build_call ext_newpic_func [| (expr builder a) ; (expr builder b) |]
+      "newpic" builder
       | A.Call (f, act) ->
          let (fdef, fdecl) = StringMap.find f function_decls in
 	 let actuals = List.rev (List.map (expr builder) (List.rev act)) in
