295936a7476ebcf3f2278af6a960aa3e71842d9d
alextrax <luciferxiii@gmail.com>
2016-04-29 12:25:21 -0400

    Fix minus int problem

--- microc-llvm/microc.ml
+++ microc-llvm/microc.ml
@@ -17,11 +17,10 @@ let _ =
 (*
 print_string (Ast.string_of_program ast); 
 *)
-print_string(Llvm.string_of_llmodule m);;
-(*  match action with
-    Ast -> print_string (Ast.string_of_program ast)
-  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+(*print_string(Llvm.string_of_llmodule m);;*)
+  match action with
+    LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
   | Compile -> let m = Codegen.translate ast in
     Llvm_analysis.assert_valid_module m;
     print_string (Llvm.string_of_llmodule m)
- *)
+ 
