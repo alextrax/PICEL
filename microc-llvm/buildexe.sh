ba7a1b61768491699442547a487bb861ff44cbe5
RuiLu <rlu0213@hotmail.com>
2016-05-03 17:27:34 -0400

    Rui

--- microc-llvm/buildexe.sh
+++ microc-llvm/buildexe.sh
@@ -2,5 +2,5 @@ cp ./libpic/convolution.pic source.pic
 cat ./test/conv.pic >> source.pic
 ./microc.native < source.pic > tmp.ll
 llc -filetype=obj tmp.ll 
-llvm-g++ tmp.o bitmap/bmplib.o 
+g++ tmp.o bitmap/bmplib.o 
 
