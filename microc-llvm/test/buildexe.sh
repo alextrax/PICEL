101b18d1a2fb81920bbee363b7151eb00ebd73f5
alextrax <luciferxiii@gmail.com>
2016-05-03 15:51:17 -0400

    Update buildexe.sh

--- microc-llvm/test/buildexe.sh
+++ microc-llvm/test/buildexe.sh
@@ -1,6 +1,9 @@
 cp ../libpic/convolution.pic source.pic
+cat ../libpic/gray.pic >> source.pic
 cat ./conv.pic >> source.pic
 ../microc.native < source.pic > tmp.ll
-llc -filetype=obj tmp.ll 
-llvm-g++ tmp.o ../bitmap/bmplib.o 
+#llc -filetype=obj tmp.ll 
+opt -mem2reg -S tmp.ll > tmp_opt.ll
+llc -filetype=obj tmp_opt.ll
+llvm-g++ tmp_opt.o ../bitmap/bmplib.o
 
