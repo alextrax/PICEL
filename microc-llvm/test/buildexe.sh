0207e93af23558381389e6da092ff4a57c3ef8e6
jxsdfzlc <jxsdfzlc@gmail.com>
2016-05-04 13:46:23 -0400

    modify test/buildexe.sh

--- microc-llvm/test/buildexe.sh
+++ microc-llvm/test/buildexe.sh
@@ -1,5 +1,6 @@
 cp ../libpic/convolution.pic source.pic
 cat ../libpic/gray.pic >> source.pic
+<<<<<<< HEAD
 cat $1 >> source.pic
 ../picel.native < source.pic > tmp.ll
 #llc -filetype=obj tmp.ll 
@@ -7,3 +8,11 @@ opt -mem2reg -S tmp.ll > tmp_opt.ll
 llc -filetype=obj tmp_opt.ll
 g++ tmp_opt.o ../bitmap/bmplib.o
 
+=======
+cat ./conv.pic >> source.pic
+../microc.native < source.pic > tmp.ll
+#llc -filetype=obj tmp.ll 
+opt -mem2reg -S tmp.ll > tmp_opt.ll
+llc -filetype=obj tmp_opt.ll
+llvm-g++ tmp_opt.o ../bitmap/bmplib.o
+>>>>>>> 5fdaa1c5e31138257dc167bb084e5703a8aa19f8
