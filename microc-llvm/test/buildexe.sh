304854902ebc9fffc51aa07b3320731e76f03738
jxsdfzlc <jxsdfzlc@gmail.com>
2016-05-04 13:21:49 -0400

    modify picel.ml test/buildexe.sh semant.ml

--- microc-llvm/test/buildexe.sh
+++ microc-llvm/test/buildexe.sh
@@ -1,20 +1,9 @@
 cp ../libpic/convolution.pic source.pic
-<<<<<<< HEAD
-cat ./conv.pic >> source.pic
-../picel.native < source.pic > tmp.ll
-llc -filetype=obj tmp.ll 
-<<<<<<< HEAD
-llvm-g++ tmp.o ../bitmap/bmplib.o 
-=======
 cat ../libpic/gray.pic >> source.pic
 cat $1 >> source.pic
-../microc.native < source.pic > tmp.ll
+../picel.native < source.pic > tmp.ll
 #llc -filetype=obj tmp.ll 
 opt -mem2reg -S tmp.ll > tmp_opt.ll
 llc -filetype=obj tmp_opt.ll
-llvm-g++ tmp_opt.o ../bitmap/bmplib.o
->>>>>>> origin/convol
-=======
-g++ tmp.o ../bitmap/bmplib.o 
->>>>>>> origin/semant_hello
+g++ tmp_opt.o ../bitmap/bmplib.o
 
