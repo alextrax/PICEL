8dba79ca4226bc39e93ad9efc4270e68f745ea61
alextrax <cw2952@columbia.edu>
2016-05-05 10:10:07 -0400

    Change microc to picel in buildexe.sh

--- microc-llvm/test/buildexe.sh
+++ microc-llvm/test/buildexe.sh
@@ -1,7 +1,7 @@
 cp ../libpic/convolution.pic source.pic
 cat ../libpic/gray.pic >> source.pic
 cat ./conv.pic >> source.pic
-../microc.native < source.pic > tmp.ll
+../picel.native < source.pic > tmp.ll
 #llc -filetype=obj tmp.ll 
 opt -mem2reg -S tmp.ll > tmp_opt.ll
 llc -filetype=obj tmp_opt.ll
