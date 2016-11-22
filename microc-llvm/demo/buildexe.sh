a09fbf08f9297ae2c9f15f01a358664faec3c6eb
alextrax <cw2952@columbia.edu>
2016-11-21 23:10:31 -0500

    Modification for FV project

--- microc-llvm/demo/buildexe.sh
+++ microc-llvm/demo/buildexe.sh
@@ -1,9 +1,13 @@
 cp ../libpic/convolution.pic source.pic
 cat ../libpic/gray.pic >> source.pic
 cat $1 >> source.pic
-../picel.native < source.pic > tmp.ll
+../picel.native < source.pic > _tmp.ll
+cp ./target.txt ./tmp.ll
+cat ./_tmp.ll >> tmp.ll
+rm ./_tmp.ll
 #llc -filetype=obj tmp.ll 
-opt -mem2reg -S tmp.ll > tmp_opt.ll
-llc -filetype=obj tmp_opt.ll
-llvm-g++ tmp_opt.o ../bitmap/bmplib.o
+#opt -mem2reg -S tmp.ll > tmp_opt.ll
+llvm-as-3.4 tmp.ll -o tmp.bc
+#llc -filetype=obj tmp_opt.ll
+#llvm-g++ tmp_opt.o ../bitmap/bmplib.o
 
